756ec5df6c1b3c6b51b7f7782fa4688d65e84420
Bradley Le <brabrele@gmail.com>
2022-02-03 04:08:33 -0800

    created a connection to database

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'company_db'
+    },
+    console.log("Connected to the database.")
+);
 
 inquirer
     .prompt([
