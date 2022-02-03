b79c6fb293926ce063b5597ca6f6a01a3f7644d2
Bradley Le <brabrele@gmail.com>
2022-02-03 03:53:03 -0800

    restructured the schema.sql file to correctly create tables and foreign keys, created seeds.sql to create sample database for testing and default database

--- db/schema.sql
+++ db/schema.sql
@@ -3,28 +3,35 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE employee (
+CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
-    FOREIGN KEY (id)
-    REFERENCES employee(role_id)
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
     ON DELETE SET NULL
 );
 
-CREATE TABLE department (
-    id INT,
-    name VARCHAR(30)
-    FOREIGN KEY (id)
-    REFERENCES role(department_id)
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
     ON DELETE SET NULL
 );
+
+
+
+
+
+
+
