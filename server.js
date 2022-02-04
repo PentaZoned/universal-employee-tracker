ed557505fb71f744778aa6f58d33f1f8e4fc9e1a
Bradley Le <brabrele@gmail.com>
2022-02-03 22:05:37 -0800

    added concatenation of manager's name in viewEmployee function, also made the code easier to look at

--- server.js
+++ server.js
@@ -65,7 +65,9 @@ function viewRoles() {
 
 function viewEmployeesQuery() {
     return new Promise((resolve, reject) => {
-        db.query(`SELECT employee.id AS Id, employee.first_name AS First_Name, employee.last_name as Last_name, role.title as Title, department.name AS Department, role.salary as Salary, manager.first_name AS Manager
+        db.query(`SELECT employee.id AS Id, employee.first_name AS First_Name, employee.last_name as Last_name,
+                    role.title as Title, department.name AS Department, role.salary as Salary,
+                    CONCAT(manager.first_name, " ", manager.last_name) AS Manager
                     FROM employee
                     LEFT JOIN role ON employee.role_id = role.id
                     LEFT JOIN department ON role.department_id = department.id
